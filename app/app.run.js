2944a39e452b724688b9392f8f54ceb7a2e096b1
Amanuel <amanuelb43@gmail.com>
2016-06-16 06:52:20 -0400

    Update app.run.js

--- app/app.run.js
+++ app/app.run.js
@@ -1,17 +1,18 @@
-<<<<<<< HEAD:app/app.run.js
-angular.module('ForumApp')
-  .run(function($rootScope, $location, $state) {
-=======
-(function(angular){
-    var app = angular.module('ForumApp');
-    
-      app.run(["$rootScope", "$location","$state", function($rootScope, $location,$state) {
->>>>>>> f0ad963ad1cdaef62f8fc70355d5af7e736899f7:run/authCheck.js
-    $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
-      // We can catch the error thrown when the $requireAuth promise is rejected
-      // and redirect the user back to the home page
-      if (error === "AUTH_REQUIRED") {
-        $state.go("home");
-      }
-    });
-  });
+(function(){
+  'use strict';
+  angular
+        .module('ForumApp')
+        .run(function($rootScope, $location, $state) {
+              (function(angular){
+                  angular
+                          .module('ForumApp');
+                          app.run(["$rootScope", "$location","$state", function($rootScope, $location,$state) {
+                            $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
+                              // We can catch the error thrown when the $requireAuth promise is rejected
+                              // and redirect the user back to the home page
+                              if (error === "AUTH_REQUIRED") {
+                                $state.go("home");
+                              }
+                            });
+                });
+})(angular);
