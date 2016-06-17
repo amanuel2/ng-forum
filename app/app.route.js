16ab1b1e99dc219ffdcd5f8852ef1ca60ede5bfc
Kyle Roach <kroach.work@gmail.com>
2016-06-17 10:03:08 -0400

    Dependecies fixup
    * Removed libs that were already in bower_components
    * Controllers now use '/* @ngInject */' instead of array for dependencies since we're using ngAnnotate that injects automatically
    * Moved package.json dependencies to devDependencies
    * Added script for bower install so we don't have to type it anymore. npm install will now automatically run bower install after it's finished.

---
+++ app/app.route.js
@@ -0,0 +1,105 @@
+(function (angular) {
+    angular.module('ForumApp')
+        .config(stateParams);
+
+    /* @ngInject */
+    function stateParams($stateProvider, $urlRouterProvider, $mdThemingProvider) {
+        $mdThemingProvider.theme("default").primaryPalette("blue");
+        $urlRouterProvider.otherwise('404');
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'app/components/home/home.html',
+                controller: 'homeCtrl'
+            })
+            .state('auth', {
+                url: '/auth',
+                templateUrl: 'app/components/auth/auth.html',
+                controller: 'authCtrl'
+            })
+            .state('authHome', {
+                url: '/authHome',
+                templateUrl: 'app/components/authHome/authHome.html',
+                controller: 'authHome',
+                resolve: {
+                    // controller will not be loaded until $waitForAuth resolves
+                    // Auth refers to our $firebaseAuth wrapper in the example above
+                }
+            })
+            .state('authHome.desc', {
+                url: '/desc',
+                templateUrl: 'app/components/authDesc/authDesc.html',
+                controller: 'authDescCtrl',
+                resolve: {
+                    // controller will not be loaded until $waitForAuth resolves
+                    // Auth refers to our $firebaseAuth wrapper in the example above
+                    "currentAuth": ["refService", function (refService) {
+                    }]
+                }
+            })
+            .state('authHome.topic', {
+                url: '/topic/:POST/:USERNAME',
+                templateUrl: 'app/components/topic/topicDesc.html',
+                controller: 'topicCtrl',
+                resolve: {
+                    // controller will not be loaded until $waitForAuth resolves
+                    // Auth refers to our $firebaseAuth wrapper in the example above
+                }
+            })
+            .state('authHome.profile', {
+                url: '/profile/:UID',
+                templateUrl: 'app/components/profile/profile.html',
+                controller: 'profileCtrl',
+                resolve: {
+                    // controller will not be loaded until $waitForAuth resolves
+                    // Auth refers to our $firebaseAuth wrapper in the example above
+                }
+            })
+            .state('authHome.settings', {
+                url: '/settings?UID?USERNAME',
+                templateUrl: 'app/components/settings/settings.html',
+                controller: 'settingsCtrl',
+                resolve: {
+                    // controller will not be loaded until $waitForAuth resolves
+                    // Auth refers to our $firebaseAuth wrapper in the example above
+                }
+            })
+            .state('authHome.otherUser', {
+                url: '/otherUser?DATE?UID',
+                templateUrl: 'app/components/otherUserProfile/otherUserProfile.html',
+                controller: 'otherUserProfileCtrl',
+                resolve: {
+                    // controller will not be loaded until $waitForAuth resolves
+                    // Auth refers to our $firebaseAuth wrapper in the example above
+                }
+            })
+            .state('ErrorFourOFour', {
+                url: '/404',
+                templateUrl: 'app/components/404/404.html',
+                controller: '404Controller',
+                resolve: {
+                    // controller will not be loaded until $waitForAuth resolves
+                    // Auth refers to our $firebaseAuth wrapper in the example above
+                }
+            })
+            .state('loading', {
+                url: '/loading',
+                templateUrl: 'app/components/loading/loading.html',
+                controller: 'loadingCtrl',
+                resolve: {
+                    // controller will not be loaded until $waitForAuth resolves
+                    // Auth refers to our $firebaseAuth wrapper in the example above
+                }
+            })
+            .state('authHome.otherUserPage', {
+                url: '/:USERNAME/Activity',
+                templateUrl: 'app/components/otherUserProfilePage/otherUserProfilePage.html',
+                controller: 'otherUserProfilePageCtrl',
+                resolve: {
+                    // controller will not be loaded until $waitForAuth resolves
+                    // Auth refers to our $firebaseAuth wrapper in the example above
+                }
+            });
+    }
+})(angular);
